22eb38b770fd2671993590e203bceeb7d2ed42f4
Rafael Gaudêncio de Sousa <rafael.rgsousa@gmail.com>
2020-02-24 22:51:07 +0000

    feat(file): making main methods on file service

--- services/file.js
+++ services/file.js
@@ -3,23 +3,20 @@ const fs = require('fs'),
     path = require('path'),
     _ = require('lodash');
 
+const FILE_PATH = './resources/network.txt';
 
 /**
  * 
  */
 const readFile = async (filePath) => {
 
-    const linesContent = [];
     const networkRelationship = [];
-
     return new Promise((resolve, reject) => {
-        const s = fs
+        fs
             .createReadStream(filePath)
-            .pipe(es.split())
+            .pipe(es.split()) //split stream to break on newlines
             .pipe(
-                es.mapSync((line) => {
-
-                    linesContent.push(line);
+                es.mapSync((line) => { //turn this async function into a stream
 
                     // get origin element
                     const firstPartLineSplitContent = line.split('->');
@@ -27,9 +24,10 @@ const readFile = async (filePath) => {
                     // get target element and latency size
                     const secondPartLineSplitContent = firstPartLineSplitContent[1].split(' ');
 
+                    // create an object representing the line
                     networkRelationship.push({
-                        origin: firstPartLineSplitContent[0],
-                        target: secondPartLineSplitContent[0],
+                        origin: firstPartLineSplitContent[0], // device 'father' of this device
+                        target: secondPartLineSplitContent[0], // device 'child' of this device
                         latency: Number(secondPartLineSplitContent[1])
                     });
 
@@ -40,7 +38,8 @@ const readFile = async (filePath) => {
                 .on('end', () => {
 
                     console.log('Read entire file.');
-
+                    // sort by target
+                    networkRelationship.sort((a, b) => (a.target > b.target) ? 1 : ((b.target > a.target) ? -1 : 0));
                     resolve(networkRelationship);
                 })
             )
@@ -48,26 +47,54 @@ const readFile = async (filePath) => {
 }
 
 /**
- * 
+ * @description Get first node
+ * @param {*} node 
+ * @param {*} network 
+ * @returns a node from network
+ */
+const getFirstNode = (nodePositon, network) => {
+
+    const node = network[nodePositon];
+
+    // It looks for the node that doens't have a 'father' or 'origin'. 
+    const filteredNetwork = network.filter((item) => item.target === node.origin);
+
+    // veirfy if the filterResult has origin
+    if (!filteredNetwork || filteredNetwork.length <= 0) {
+        return node; // node found
+    }
+
+    // try again
+    return getFirstNode(nodePositon + 1, network);
+}
+
+
+/**
+ * @description get network from file
+ * @returns { Array } network representation
+ */
+const getNetworkFromFile = async (filePath) => {
+
+    // get network representation from file
+    return await readFile(path.resolve(filePath));
+
+}
+
+/**
+ *  @description It
  */
 const makeNetworkRepresentaiton = async () => {
 
     // get network relationship from file
-    const networkRelationship = await readFile(path.resolve('./resources/network.txt'));
+    const networkRelationship = await getNetworkFromFile(FILE_PATH);
 
-    // sort by latency
-    networkRelationship.sort((a, b) => (a.latency > b.latency) ? 1 : ((b.latency > a.latency) ? -1 : 0));
+    const firstNode = getFirstNode(0, networkRelationship);
 
-
-    // get grouped by origin
-
-    const groupedByOrigindevice = _(networkRelationship).groupBy(x => x.origin).value();
-    const groupedByTargetdevice = _(networkRelationship).groupBy(x => x.target).value();
-
+    const b = firstNode;
 }
 
 
-// makeNetworkRepresentaiton();
+makeNetworkRepresentaiton();
 
 module.exports = {
     makeNetworkRepresentaiton
