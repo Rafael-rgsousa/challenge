82b2fb851b2d44654bff12f401c0c0fb979525b9
Rafael Gaudêncio de Sousa <rafael.rgsousa@gmail.com>
2020-02-26 01:27:05 +0000

    test(device)

--- test/devices.js
+++ test/devices.js
@@ -31,16 +31,47 @@ describe('Testing device service', () => {
             });
     });
 
-    // it('Needs to get a list of paths beginning with the first node', (done) => {
+    it('Needs to get a list of paths beginning with a node', (done) => {
 
-    //     chai.request(app)
-    //     .get('/paths/')
-    //     .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.body.should.be.a('object');
-    //         res.body.should.have.property('starting_node');
-    //         res.body.should.have.property('paths');
-    //         done();
-    //     });
-    // });
+        chai.request(app)
+        .get('/paths/SBZG')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('paths');
+            res.body.paths.should.be.a('array');
+            done();
+        });
+    });
+
+
+    it('Needs to get the highest time beginning with a node', (done) => {
+
+        chai.request(app)
+        .get('/highest-time/SBZG')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('path');
+            res.body.should.have.property('latency');
+            res.body.latency.should.be.a('number');
+            res.body.path.should.be.a('array');
+            done();
+        });
+    });
+
+    it('Needs to get the lowest time beginning with a node', (done) => {
+
+        chai.request(app)
+        .get('/lowest-time/SBZG')
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('path');
+            res.body.should.have.property('latency');
+            res.body.latency.should.be.a('number');
+            res.body.path.should.be.a('array');
+            done();
+        });
+    });
 });
